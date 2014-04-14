b149f2326cecaa8e56f7c3894b6a9678729f8592
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2014-04-14 11:09:29 +0700

    init

---
+++ rxvote.go
@@ -0,0 +1,46 @@
+package rxvote
+
+import "time"
+import "math/rand"
+
+type Randomizer interface {
+  Seed(n int64)
+  Intn(n int) int
+}
+
+func GetVoteResult(votes []bool) bool {
+  trueCounter, falseCounter := 0, 0
+
+  for _, vote := range votes {
+    if vote == true {
+      trueCounter++
+    } else {
+      falseCounter++
+    }
+  }
+  return trueCounter > falseCounter
+}
+
+func Vote() (votes []bool) {
+  rand.Seed(time.Now().Unix())
+  /* // sequential
+  for i := 0; i < 3; i++ {
+    time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
+    votes = append(votes, (rand.Intn(1) == 1))
+  }
+  */
+
+  // concurrency
+  ch := make(chan bool)
+  for i := 0; i < 3; i++ {
+    go func() {
+      time.Sleep(time.Duration(rand.Intn(4)) * time.Second)
+      ch <- (rand.Intn(2) == 1)
+    }()
+  }
+  for i := 0; i < 3; i++ {
+    votes = append(votes, <-ch)
+  }
+  close(ch)
+  return
+}
---
+++ rxvote_test.go
@@ -0,0 +1,39 @@
+package rxvote
+
+import "testing"
+import "time"
+import "math/rand"
+import "fmt"
+
+type MockRand rand.Rand
+func (m MockRand) Intn(n int) int {
+  return 3
+}
+func (m MockRand) Seed(n int64) {}
+
+func TestGetVoteResult(t *testing.T) {
+  tests := []struct{
+    input []bool
+    expected bool
+  }{
+    {[]bool{true, true, true}, true},
+  }
+
+  for _, test := range tests {
+    if GetVoteResult(test.input) != test.expected {
+      t.Errorf("ERROR: GetVoteResult of %q should return %q", test.input, test.expected)
+    }
+  }
+}
+
+func TestVote(t *testing.T) {
+  ch := make(chan []bool, 1)
+  timeout := time.After(4 * time.Second)
+  ch <- Vote()
+  select {
+    case ret := <-ch:
+      fmt.Println(ret)
+    case <-timeout:
+      t.Errorf("ERROR: Timeout")
+  }
+}
